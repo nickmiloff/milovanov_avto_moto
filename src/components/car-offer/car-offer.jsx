174856d2f8e13ef969701edda40363a845bb21bf
Nick <nickmiloff@users.noreply.github.com>
2021-06-04 21:28:39 +0300

    Добавляет информацию о товаре

--- src/components/car-offer/car-offer.jsx
+++ src/components/car-offer/car-offer.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 import Gallery from '../gallery/gallery';
-import CarOfferInfo from '../car-offer-info/car-offer-info';
+import OfferInfo from '../offer-info/offer-info';
 import CarOfferTabs from '../car-offer-tabs/car-offer-tabs';
 
-import {offerImages} from '../../mock';
+import {offerImages, offerDescription} from '../../mock';
 
 const CarOffer = () => {
   return (
     <section className="car-offer">
       <div className="container">
         <Gallery additionalClass="car-offer__gallery car-offer__gallery--new" photos={offerImages} />
-        <CarOfferInfo />
+        <OfferInfo {...offerDescription} />
         <CarOfferTabs />
       </div>
     </section>
