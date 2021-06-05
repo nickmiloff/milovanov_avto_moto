6a1863f085a6ce042885e523a84e98f9a3c4e1a0
Nick <nickmiloff@users.noreply.github.com>
2021-06-05 23:43:13 +0300

    Добавляет подвал

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,25 +9,25 @@ const Header = () => {
         <a href="/" className="header__logo logo" >
           <img className="logo__img" src={logoImg} alt="Логотип интернет-магазина - Avto-Moto" width="134" height="55" />
         </a>
-        <nav className="header__nav main-nav">
-          <ul className="main-nav__list">
-            <li className="main-nav__item">
-              <a href="" className="main-nav__link">
+        <nav className="header__nav nav">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="" className="nav__link">
                 Автомобили
               </a>
             </li>
-            <li className="main-nav__item">
-              <a href="" className="main-nav__link">
+            <li className="nav__item">
+              <a href="" className="nav__link">
                 Контакты
               </a>
             </li>
-            <li className="main-nav__item">
-              <a href="" className="main-nav__link">
+            <li className="nav__item">
+              <a href="" className="nav__link">
                 Услуги
               </a>
             </li>
-            <li className="main-nav__item">
-              <a href="" className="main-nav__link">
+            <li className="nav__item">
+              <a href="" className="nav__link">
                 Вакансии
               </a>
             </li>
