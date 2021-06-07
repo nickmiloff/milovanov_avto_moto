70d238ec5fa3dc6948bd6539b8e639e454cdfc1f
Nick <nickmiloff@users.noreply.github.com>
2021-06-07 22:56:33 +0300

    Добавляет модально окно

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import ReviewsItem from '../reviews-item/reviews-item';
+import ReviewsForm from '../reviews-form/reviews-form';
+import Modal from '../modal/modal';
 
 const Reviews = ({
   items
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="reviews">
-      <a href="" className="reviews__review-button link link--secondary">
+      <button
+        href=""
+        className="reviews__review-button link link--secondary"
+        type="button"
+        onClick={() => {
+          setIsModalOpen(() => true);
+        }}
+      >
         оставить отзыв
-      </a>
+      </button>
       <ul className="reviews__list reviews__list--main">
         {
           items.map((el, index) => <ReviewsItem key={`reviews-item-${index}`} {...el} />)
         }
       </ul>
+      <Modal
+        isOpen={isModalOpen}
+        onCancel={() => {
+          setIsModalOpen(() => false);
+        }}
+      >
+        <ReviewsForm
+          onSubmit={() => {
+            setIsModalOpen(() => false);
+          }}
+        />
+      </Modal>
     </div>
   );
 };
